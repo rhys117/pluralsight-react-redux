d103d2817b82fbdff0cf30193aabcf2b448d7918
rhys117 <rhys117@gmail.com>
2019-03-06 18:58:33 +1000

    completed module 5 - foundation of app

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import Layout from './components/Layout';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import CoursesPage from './components/course/CoursesPage';
 
 export default (
   <Route path="/" component={Layout}>
     <IndexRoute component={HomePage}/>
+    <Route path="courses" component={CoursesPage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
