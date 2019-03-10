bdc6c61b81ca45d9ffb77690fc436dbe82908851
rhys117 <rhys117@gmail.com>
2019-03-10 17:54:26 +1000

    select and text input components added

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import Layout from './components/Layout';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
+import ManageCoursePage from './components/course/ManageCoursePage';
 
 export default (
   <Route path="/" component={Layout}>
     <IndexRoute component={HomePage}/>
     <Route path="courses" component={CoursesPage} />
+    <Route path="course" component={ManageCoursePage} />
+    <Route path="course/:id" component={ManageCoursePage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
