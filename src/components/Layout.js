9923362b3071bbedef088ac4a5bfb26112879fe4
rhys117 <rhys117@gmail.com>
2019-03-11 22:03:16 +1000

    ajax status and error handling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,15 @@
 // This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 
 class Layout extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <Header />
+        <Header
+          loading={this.props.loading}
+        />
         {this.props.children}
       </div>
     );
@@ -14,7 +17,14 @@ class Layout extends React.Component {
 }
 
 Layout.propTypes = {
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
-export default Layout;
+function mapStateToProps(state, ownProps) {
+ return {
+   loading: state.ajaxCallsInProgress > 0
+ };
+}
+
+export default connect(mapStateToProps)(Layout);
